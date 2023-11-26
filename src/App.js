a657db4eedf7834bb1a074170b674480a9318cf0
Prateek Sogra <prateeksogra18@gmail.com>
2023-11-26 18:32:48 +0530

    added inital files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Query from './TextImage.js';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [imageSrc, setImageSrc] = useState(null);
+  const [errorState, setErrorState] = useState(false);
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleFetchImage = async (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+
+    try {
+      const response = await Query({ "inputs": query });
+      const blobUrl = URL.createObjectURL(response);
+      setImageSrc(blobUrl);
+      setErrorState(false);
+    } catch (error) {
+      setErrorState(true);
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFetchImage} className="row g-3">
+        <input type="text" id="queryInput" value={query} onChange={handleQueryChange} placeholder="Send messages..." autoComplete="off"/>
+        <button type="submit" className="btn btn-primary">Create</button>
+      </form>
+      {imageSrc && <img src={imageSrc}/>}
+      {errorState && <div><p>Error loading image</p></div>}
     </div>
   );
 }
